60538e6ef591251130b089a714ec6ae1e99f08ad
bhivgadearav <hazeifygameplay5927@gmail.com>
2026-01-02 20:43:43 +0530

    wrote color theme and text styles

---
+++ packages/theme/src/index.ts
@@ -0,0 +1,20 @@
+// Color Theme: https://coolors.co/palette/000000-14213d-fca311-e5e5e5-ffffff
+// Color Theme Visualized: https://coolors.co/visualizer/000000-14213d-fca311-e5e5e5-ffffff
+const colors = {
+  primary: "#14213D",
+  secondary: "#FCA311",
+  gray: "#E5E5E5",
+  dark: "#000000",
+  background: "#FFFFFF"
+}
+
+const textStyles = {
+  h1: "scroll-m-20 text-center text-4xl font-bold tracking-tight text-balance",
+  h2: "scroll-m-20 pb-2 text-3xl font-semibold tracking-tight",
+  h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
+  h4: "scroll-m-20 text-xl font-semibold tracking-tight",
+  blockquote: "border-l-2 pl-6 italic",
+  inlineCode:
+    "bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+  lead: "text-muted-foreground text-xl",
+}
